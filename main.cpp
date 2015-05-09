8f6028f39419d0c4b7d9ad60f8eff59a8cc14229
Dilara <manoj.pesit@gmail.com>
2015-05-09 11:50:30 +0200

    Tested implimention of Grid Class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
+#include "grid.h"
+
 #include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+#include <vector>
+#include <assert.h>
+
 
 using namespace std;
 
-int main()
+size_t ll,nov;
+
+int main(int argc , char **argv)
 {
+
+if(argc != 3){ cout << "Improper input " << endl ; exit(EXIT_FAILURE);}
+
+if(argc == 3){ 
+ll = atoi(argv[1]);
+nov = atoi(argv[2]); }
+
+Grid A(ll);
+
+
     cout << "Hello world!" << endl;
     return 0;
 }
