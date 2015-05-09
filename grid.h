8a1d167b8f5c73ff9373c19f27b4bc60b33012b5
Dilara <manoj.pesit@gmail.com>
2015-05-09 11:50:05 +0200

    Wrote member functions

--- grid.h
+++ grid.h
@@ -1,15 +1,39 @@
 #ifndef GRID_H
 #define GRID_H
 
+#include <cstdlib>
+#include <vector>
+typedef double real ;
 
 class Grid
 {
     public:
-        Grid();
-        virtual ~Grid();
+	explicit Grid();
+	explicit Grid(size_t level);
+		 Grid(const Grid& grid);
+	       
         Grid& operator=(const Grid& other);
-    protected:
-    private:
+
+	//Get functions
+	inline size_t xsize();
+	inline size_t ysize();
+	inline real hsize();   
+
+	//Access functions
+	inline real & operator()(size_t i, size_t j);
+	inline real & operator()(size_t i, size_t j) const;    
+		
+	void fill (real value);
+	
+	
+
+   private:
+	size_t y_ ;
+	size_t x_ ;
+	int l;
+	real h_ ;
+	std::vector<real> v_ ;
+
 };
 
 #endif // GRID_H
