93384cebac41dfd7650282b792380a952ceac4ae
Dilara <manoj.pesit@gmail.com>
2015-05-15 07:48:27 +0200

    Smoother Function

--- mgsolver.cpp
+++ mgsolver.cpp
@@ -1,14 +1,12 @@
 #include "mgsolver.h"
 #include "grid.h"
+#include <iostream>
+
+using namespace std;
 
 MGSolve::MGSolve()
 {
     //ctor
-}
-
-MGSolve::~MGSolve()
-{
-    //dtor
 }
 
 MGSolve& MGSolve::operator=(const MGSolve& rhs)
@@ -17,3 +15,82 @@ MGSolve& MGSolve::operator=(const MGSolve& rhs)
     //assignment operator
     return *this;
 }
+
+void MGSolve::setRestrictionStencil(const Stencil& stencil){
+	
+	R_ = stencil;
+	R_.C = 0.25;
+	R_.N = 0.125;
+	R_.W = 0.125;
+	R_.E = 0.125;
+	R_.S = 0.125;
+	R_.NW = 0.0625;
+	R_.NE = 0.0625;
+	R_.SW = 0.0625;
+	R_.SE = 0.0625;
+}
+
+void MGSolve::setSmootherStencil(const Stencil& stencil){
+	
+	RB_ = stencil;
+	RB_.N = 0.25; 
+	RB_.S = 0.25;
+	RB_.W = 0.25;
+	RB_.E = 0.25;
+}
+
+void MGSolve::setInterpolationStencil(const Stencil& stencil){
+	
+	I_ = stencil;
+	I_.C = 1.0;
+	I_.N = 0.5;
+	I_.W = 0.5;
+	I_.E = 0.5;
+	I_.S = 0.5;
+	I_.NW = 0.25;
+	I_.NE = 0.25;
+	I_.SW = 0.25;
+	I_.SE = 0.25;	
+}
+
+void MGSolve::smooth(Grid& u,Grid& f,size_t iter){
+	//iter = 2;
+	const size_t size= sizeof(u.xsize()* u.xsize());
+	std::cout << size << std::endl;;
+	real h2 = u.h2size();
+	std::cout << h2 << std::endl;;
+	//std::cout <<"xsize:"<<<< xsize() << std::endl;
+	for(iter = 0 ; iter < 2 ; ++iter){
+
+	for(size_t j=1;j<size-1;++j)
+		for(size_t i= 1 + (j+1)%2 ; i <size-1;++i){
+
+		u[i + size*j] = 0.25 * ( h2 * f[i +size*j] 
+					+ u[(i-1) +size*j]
+					+ u[(i+1) +size*j]
+					+ u[i +size*(j+1)]
+					+ u[i +size*(j-1)]);} 
+		
+	for(size_t j=1;j<size-1;++j)
+		for(size_t i= 1 + (j)%2 ; i <size-1;++i){
+
+		u[i + size*j] = 0.25 * ( h2 * f[i +size*j] 
+					+ u[(i-1) +size*j]
+					+ u[(i+1) +size*j]
+					+ u[i +size*(j+1)]
+					+ u[i +size*(j-1)]);}
+
+
+	}
+}
+
+MGSolve::~MGSolve()
+{
+    //dtor
+}
+
+
+
+
+
+
--- mgsolver.h
+++ mgsolver.h
@@ -3,16 +3,49 @@
 #include "grid.h"
 
 #include <vector>
+#include <iostream>
+typedef double real;
+
+using namespace std;
+
+struct Stencil{
+	
+	//explicit inline stencil();
+	real C;
+	real N;
+	real E;
+	real W;
+	real S;
+	real NW;
+	real NE;
+	real SW;
+	real SE;
+};
+
 class MGSolve
 {
     private:
-	typedef std::vector<Grid> Grids;	
+	//Grid* Grids;
+	 //Grids* = new real [Grid(levels_)*Grid(levels_)]
+	typedef std::vector<Grid> Grids;
+		
     public:
         MGSolve();
         virtual ~MGSolve();
         MGSolve& operator=(const MGSolve& other);
-    
+	void setSmootherStencil(const Stencil& stencil);
+	void setRestrictionStencil(const Stencil& stencil);
+    	void setInterpolationStencil(const Stencil& stencil);
     private:
+	void smooth(Grid& u,Grid& f,size_t iter);
+	size_t levels_ ;
+	//Grids = new real*
+	Grids u_ ;
+	Grids f_ ;
+	Grids r_ ;
+	Stencil RB_ ;
+	Stencil R_ ;
+	Stencil I_ ;
 };
 
 #endif // MGSOLVE_H
